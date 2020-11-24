ffb9d5e1df9de8910733c07e0513c7e9d38f4b60
Xenolphthalein <lamatscht@gmail.com>
2020-11-24 01:05:21 +0100

    initial commit

---
+++ Main.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Flow.Launcher.Plugin;
+
+namespace Flow.Launcher.Plugin.ClipboardHistory
+{
+    public class ClipboardHistory : IPlugin
+    {
+        private const int MaxDataCount = 300;
+        //private readonly KeyboardSimulator keyboardSimulator = new KeyboardSimulator(new InputSimulator());
+        private PluginInitContext context;
+        List<string> dataList = new List<string>();
+
+        public List<Result> Query(Query query)
+        {
+            var results = new List<Result>();
+            List<string> displayData;
+
+            if (query.Terms.Length == 0)
+            {
+                displayData = dataList;
+            }
+            else
+            {
+                displayData = dataList.Where(i => i.ToLower().Contains(query.SecondToEndSearch.ToLower()))
+                        .ToList();
+            }
+
+            results.AddRange(displayData.Select(o => new Result
+            {
+                Title = o.Trim(),
+                IcoPath = "Images\\clipboard.png",
+                Action = c =>
+                {
+                    try
+                    {
+                        System.Windows.Forms.Clipboard.SetText(o);
+                        return true;
+                    }
+                    catch (Exception e)
+                    {
+                        context.API.ShowMsg("Error", e.Message, null);
+                        return false;
+                    }
+                }
+            }).Reverse());
+            return results;
+        }
+
+        public void Init(PluginInitContext context)
+        {
+            this.context = context;
+            ClipboardMonitor.OnClipboardChange += ClipboardMonitor_OnClipboardChange;
+            ClipboardMonitor.Start();
+        }
+
+        void ClipboardMonitor_OnClipboardChange(ClipboardFormat format, object data)
+        {
+            if (format == ClipboardFormat.Html ||
+                format == ClipboardFormat.SymbolicLink ||
+                format == ClipboardFormat.Text ||
+                format == ClipboardFormat.UnicodeText)
+            {
+                if (data != null && !string.IsNullOrEmpty(data.ToString().Trim()))
+                {
+                    if (dataList.Contains(data.ToString()))
+                    {
+                        dataList.Remove(data.ToString());
+                    }
+                    dataList.Add(data.ToString());
+
+                    if (dataList.Count > MaxDataCount)
+                    {
+                        dataList.Remove(dataList.Last());
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Flow Launcher Clipboard History Plugin
+Ported from https://github.com/Wox-launcher/Wox.Plugin.ClipboardManager
+
+## Keyword
+cb (stands for ClipBoard)
+
+## About
+At the moment it's just a plain port of the original Plugin with a tiny bit of changes.
+If you have any suggestions to make it a better plugin feel free to open an issue.
